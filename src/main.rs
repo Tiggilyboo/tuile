889c8fda5d7c8d8ffdb5a9c2ee575185b6a970b2
Simon Willshire <me@simonwillshire.com>
2024-02-25 22:57:02 +0100

    Initial minimal deps for mlir, libc, and nom
    - nom: frontend AST parser (already in deps for mlir-sys)
    - mlir-sys: llvm MLIR backend for codegen
    - libc: no_std to do sys-calls to

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![no_main]
+#![no_std]
+
+extern crate libc;
+extern crate mlir_sys as mlir;
+
+#[no_mangle]
+pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    // Since we are passing a C string the final null character is mandatory.
+    const HELLO: &'static str = "Hello, world!\n\0";
+    unsafe {
+        libc::printf(HELLO.as_ptr() as *const _);
+    }
+    0
+}
